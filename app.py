2aac86e73b4ee6a6799f8104cbde940c386c7698
faust65 <151982135+faust65@users.noreply.github.com>
2025-11-06 16:12:56 +0900

    dkr

--- app.py
+++ app.py
@@ -28,6 +28,22 @@ def process():
                 message=f"극단적 성공({g})"
         
     return jsonify({'message':message})
+
+@app.route('/process2', methods=['POST'])
+def process2():
+    data=request.get_json()
+    stat=int(data.get('stat', '0'))
+    
+    g=random.randrange(1, 101)
+
+    if(g==99 or g==100):
+        message=f"대실패({g})"
+    elif((stat==1 and g<=20) or (stat==2 and g<=40) or (stat==3 and g<=50) or (stat==4 and g<=60) or (stat==5 and g<=80)):
+        message=f"성공({g})"
+    else:
+        message=f"실패({g})"
+    
+    return jsonify({'message':message})
     
 if __name__ == '__main__':
     app.run(debug=True)
